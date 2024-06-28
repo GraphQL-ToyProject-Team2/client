07157af8e5affd7830f100ce8dff06207daf5e16
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-06-28 22:01:26 +0900

    feat: WishList 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Main from './pages/Main';
 import Detail from './pages/Detail';
 import styled from 'styled-components';
 import GlobalStyle from './styles/globalStyles';
+import WishList from './pages/WishList';
 
 const MobileContainer = styled.div`
   font-family: 'Pretendard Variable';
@@ -23,6 +24,7 @@ function App() {
   const router = createBrowserRouter([
     { path: '/', element: <Main /> },
     { path: '/detail/:id', element: <Detail /> },
+    { path: '/wishlist', element: <WishList /> },
   ]);
   return (
     <MobileContainer>
---
+++ src/pages/WishList.tsx
@@ -0,0 +1,169 @@
+import { gql, useQuery } from '@apollo/client';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { ChevronLeft, ShareOutlined, Favorite, FavoriteBorder } from '@material-ui/icons';
+
+const GET_ACCOMMODATIONS = gql`
+  query GetAccommodations {
+    accommodations {
+      id
+      title
+      description
+      price
+      host {
+        name
+      }
+      images
+      isLiked
+    }
+  }
+`;
+
+interface WishListAccommodation {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  host: {
+    id: string;
+    name: string;
+  };
+  images: string[];
+  isLiked: boolean;
+}
+
+interface Accommodations {
+  accommodations: WishListAccommodation[];
+}
+
+const WishList = () => {
+  const navigate = useNavigate();
+  const { loading, error, data } = useQuery<Accommodations>(GET_ACCOMMODATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const accommodations = data?.accommodations;
+
+  return (
+    <StWrapper>
+      <Header>
+        <button onClick={() => navigate(-1)}>
+          <StyledChevronLeft fontSize="large" />
+        </button>
+        <></>
+      </Header>
+      <StRoomListContainer>
+        {accommodations
+          ?.filter((item) => item.isLiked)
+          .map((accommodation) => {
+            const { id, title, price, host, images } = accommodation;
+
+            return (
+              <StRoomCard key={id} onClick={() => navigate(`/detail/${id}`)}>
+                <StThumbnail src={images[0]} />
+                <div>
+                  <h1>{title}</h1>
+                  <h2>호스트 : {host.name}님</h2>
+                  <p>₩{price} /인</p>
+                </div>
+              </StRoomCard>
+            );
+          })}
+      </StRoomListContainer>
+    </StWrapper>
+  );
+};
+
+export default WishList;
+
+const StWrapper = styled.main`
+  width: 100%;
+`;
+
+const Img = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
+const StRoomListContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+
+  margin-top: 24px;
+  padding: 0 24px 0 24px;
+`;
+
+const StRoomCard = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  padding: 20px;
+
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  transition:
+    transform 0.3s,
+    box-shadow 0.3s;
+  border-radius: 12px;
+
+  cursor: pointer;
+
+  &:hover {
+    transform: translateY(-15px);
+    box-shadow: 0px 8px 12px rgba(0, 0, 0, 0.2);
+  }
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+
+    & > h1 {
+      font-weight: 500;
+      font-size: 18px;
+    }
+
+    & > h2 {
+      font-size: 15px;
+      font-size: 15px;
+
+      color: #6a6a6a;
+    }
+
+    & > p {
+      font-weight: 400;
+      font-size: 15px;
+    }
+  }
+`;
+
+const StThumbnail = styled.img`
+  width: 100%;
+  height: 287px;
+  object-fit: cover;
+  border-radius: 12px;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 64px;
+  padding: 0 16px;
+  z-index: 1;
+  border-bottom: 1px solid rgb(235 235 233);
+  width: 100%;
+`;
+
+const StyledChevronLeft = styled(ChevronLeft)`
+  font-size: 16px;
+`;
+
+const Interaction = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+`;
