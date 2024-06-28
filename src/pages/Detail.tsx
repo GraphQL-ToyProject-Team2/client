aca5af8e8b52da01959a9714c6b5da2a932b5a9d
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-06-28 12:56:30 +0900

    feat: detail accommodation api 연결

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,5 +1,31 @@
+import { gql, useQuery } from '@apollo/client';
+
+const GET_DETAIL = gql`
+  query GetAccommodation($id: ID!) {
+    accommodation(id: $id) {
+      title
+      description
+      price
+      host {
+        id
+        name
+        avatar
+        bio
+      }
+      location
+      images
+      isLiked
+    }
+  }
+`;
+
 const Detail = () => {
-  return <div>Detail</div>;
+  const { loading, error, data } = useQuery(GET_DETAIL, { variables: { id: 1 } });
+  console.log(data);
+  const accommodation = data?.accommodation;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return <></>;
 };
 
 export default Detail;
