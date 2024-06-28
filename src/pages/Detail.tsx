b43928defa3691ea0f2648f8e68132e8260e83e9
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-06-28 13:07:32 +0900

    feat: header publishing

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,4 +1,6 @@
 import { gql, useQuery } from '@apollo/client';
+import { ChevronLeft, Favorite, FavoriteBorder, ShareOutlined } from '@material-ui/icons';
+import styled from 'styled-components';
 
 const GET_DETAIL = gql`
   query GetAccommodation($id: ID!) {
@@ -25,7 +27,39 @@ const Detail = () => {
   const accommodation = data?.accommodation;
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-  return <></>;
+  return (
+    <>
+      <Header>
+        <StyledChevronLeft fontSize="large" />
+        <Interaction>
+          <ShareOutlined />
+          {accommodation.isLiked ? <Favorite fontSize="large" /> : <FavoriteBorder />}
+        </Interaction>
+      </Header>
+    </>
+  );
 };
 
 export default Detail;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 64px;
+  padding: 0 16px;
+  z-index: 1;
+  border-bottom: 1px solid rgb(235 235 233);
+  width: 100%;
+`;
+
+const StyledChevronLeft = styled(ChevronLeft)`
+  font-size: 16px;
+`;
+
+const Interaction = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+`;
