90ef45d9dddfbbf9c99f366dc551b28b6cc59082
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-06-28 12:34:55 +0900

    init: styled-components 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Main from './pages/Main';
 import Detail from './pages/Detail';
+import styled from 'styled-components';
+import GlobalStyle from './styles/globalStyles';
+
+const MobileContainer = styled.div`
+  display: flex;
+
+  position: relative;
+  flex-direction: column;
+  align-items: center;
+
+  margin: 0 auto;
+  background-color: #fff;
+
+  max-width: 375px;
+  min-height: 100vh;
+`;
 
 function App() {
   const router = createBrowserRouter([
     { path: '/', element: <Main /> },
     { path: '/detail/:id', element: <Detail /> },
   ]);
-  return <RouterProvider router={router} />;
+  return (
+    <MobileContainer>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </MobileContainer>
+  );
 }
 
 export default App;
